4c3fefad428205cb38f51ad7bba21953b19315b5
Amine Rifi <amineprog@gmail.com>
2017-04-09 20:53:03 +0100

    init

---
+++ application/controllers/WebHook.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Description of WebHook
+ *
+ * @author rifi
+ */
+class WebHook extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        echo "this is the webHook";
+    }
+
+}
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Description of MY_Controller
+ *
+ * @author Developper: AmineRifi
+ */
+class MY_Controller extends CI_Controller {
+
+    public $data = array();
+
+    //private $myKey;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
