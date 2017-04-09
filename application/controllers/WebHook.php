12dba89b6f53982ece45137b5e6f53f5cffded80
Amine Rifi <amineprog@gmail.com>
2017-04-09 21:08:20 +0100

    init webHook

--- application/controllers/WebHook.php
+++ application/controllers/WebHook.php
@@ -13,6 +13,19 @@ class WebHook extends MY_Controller {
 
     public function index() {
         echo "this is the webHook";
+        $access_token = "EAAFqBX7yOZCwBALfcYDxVwnUuFsbk4T0c0hgGkZAqYsI3bj29bIZAXA1lZA30KzZAGW6ZCzaOIdXN2YWzQYwVZBauewM1vSErZA7nZBwH9TKqjjeU3PrIh0ikU67bSQqvU6fmZBumVK95z4tXSv1iRhDvm9wDZCCKQS27G43bFw1qyfzgZDZD";
+        $verify_token = "smartybot_my_messenger_bot_hehehehe";
+        $hub_verify_token = null;
+
+        if ($this->input->get('hub_challenge')) {
+            $challenge = $this->input->get('hub_challenge');
+            $hub_verify_token = $this->input->get('hub_verify_token');
+        } else {
+            die("bad Request");
+        }
+        if ($hub_verify_token === $verify_token) {
+            echo $challenge;
+        }
     }
 
 }
