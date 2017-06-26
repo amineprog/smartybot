31055aca71298a21dc995f497b8f194fc1ae070d
Amine Rifi <amineprog@gmail.com>
2017-06-26 22:32:01 +0000

    ok

--- application/third_party/queue/bin.php
+++ application/third_party/queue/bin.php
@@ -5,7 +5,7 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use Queue\Chat;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/queue/vendor/autoload.php';
 
 $server = IoServer::factory(
                 new HttpServer(
