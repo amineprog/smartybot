d18a264bf3819afdc7f9df4a3091940fbe72a63a
Amine Rifi <amineprog@gmail.com>
2017-07-25 21:07:10 +0000

    ok

--- application/controllers/WebHook.php
+++ application/controllers/WebHook.php
@@ -14,6 +14,7 @@ class WebHook extends MY_Controller {
     public function index() {
         //echo "this is the webHook";
         $access_token = "EAAFqBX7yOZCwBALfcYDxVwnUuFsbk4T0c0hgGkZAqYsI3bj29bIZAXA1lZA30KzZAGW6ZCzaOIdXN2YWzQYwVZBauewM1vSErZA7nZBwH9TKqjjeU3PrIh0ikU67bSQqvU6fmZBumVK95z4tXSv1iRhDvm9wDZCCKQS27G43bFw1qyfzgZDZD";
+        $access_token_tdi_messenger_page = "EAAFqBX7yOZCwBACHVWGq42CHfUVe1KJVhnHemGsgsFv8DBoxJQGuee0lgUR96V2nBZBC3ZBF7fTTEOpFx3D8vpzhXDXNikMy0MiNPszXRNgtTxslVdpIIDTF88YkwaSHl2IHJTCHys1Eh1UZCbgZBlIp1I2OlqlL2joqocDH7OvaMjVLm5ZA8l";
         $verify_token = "smartybot_my_messenger_bot_hehehehe";
         $hub_verify_token = null;
 
@@ -26,8 +27,10 @@ class WebHook extends MY_Controller {
         if ($hub_verify_token === $verify_token) {
             echo $challenge;
         }
+        $input = json_decode(file_get_contents('php://input'), true);
+
         echo $file = realpath(APPPATH . '../' . '/response.txt');
-        file_put_contents($file, 'OK time : => ' . time());
+        file_put_contents($file, $input);
         if ($this->input->post()) {
             file_put_contents($file, json_encode($this->input->post()));
         }
